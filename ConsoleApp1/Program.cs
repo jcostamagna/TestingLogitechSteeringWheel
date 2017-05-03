e14a8f3729e44d34348be0aa2eaa5d365fbdefe8
jcostamagna <j_costamagna@hotmail.com>
2017-05-03 14:43:57 +0100

    Initial commit with a hello world program using the steering wheel

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+                Thread.Sleep(2000);
+                LogitechGSDK.LogiSteeringInitialize(true);
+                for (int i = 0; i < 1000000; i++)
+             {
+                    //All the test functions are called on the first device plugged in(index = 0) 
+                    LogitechGSDK.LogiSteeringInitialize(true);
+                    if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
+                {
+                    if (LogitechGSDK.LogiIsPlaying(0, LogitechGSDK.LOGI_FORCE_SPRING))
+                    {
+                        LogitechGSDK.LogiStopSpringForce(0);
+                    }
+                    else
+                    {
+                        LogitechGSDK.LogiPlaySpringForce(0, 50, 50, 50);
+                    }
+                    }
+                else
+                {
+                    LogitechGSDK.LogiPlayDirtRoadEffect(0, 100);
+                }
+            }
+            Console.WriteLine("The program it's going to stop! ");
+            LogitechGSDK.LogiSteeringShutdown();
+            }
+        }
+    }
+}
